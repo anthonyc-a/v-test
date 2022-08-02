c01aa1df3d47c99c658fdc1430620d82ee40c2a1
Anthony <anthonyc.animba@gmail.com>
2022-08-02 03:24:22 +0100

    feat: modal changes, README changes

--- components/Layout/SignIn/Signin.tsx
+++ components/Layout/SignIn/Signin.tsx
@@ -9,7 +9,7 @@ const Signin = ({ setSignInActive }: any) => {
         }}
         className="bg absolute top-0 left-0 w-full h-full bg-black opacity-70 -z-10"
       />
-      <div className="modal absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 w-4/5 h-96 p-4 bg-white">
+      <div className="modal absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 w-80 h-96 p-4 bg-white">
         <h2 className="text-xl">Log In</h2>
       </div>
     </div>
